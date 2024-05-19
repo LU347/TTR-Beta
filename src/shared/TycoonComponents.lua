50da7925eea375819fdb459dfb9702f71894e2ad
Elisha Lucile O. <ongelishalucile@gmail.com>
2024-05-18 23:51:33 -0500

    Upgradeable Plots
    needs tweaking

---
+++ src/shared/TycoonComponents.lua
@@ -0,0 +1,11 @@
+local TemplatePlot = game.Workspace.TemplatePlot --replace
+local plots = TemplatePlot.Plots
+
+return {
+    ["PlotA"] = plots.PlotA,
+    ["PlotB"] = plots.PlotB,
+    ["PlotC"] = plots.PlotC,
+    ["PlotD"] = plots.PlotD,
+    ["PlotE"] = plots.PlotE,
+    ["PlotF"] = plots.PlotF
+}
